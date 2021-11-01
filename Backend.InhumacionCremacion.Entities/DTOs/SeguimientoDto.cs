758baa7b658b2e4051b9c7e6c194d5f31718317a
Sebastian Ordoñez Rubiano <ingeniero.sor@gmail.com>
2021-10-31 20:45:53 -0500

    ajuste objeto

---
+++ Backend.InhumacionCremacion.Entities/DTOs/SeguimientoDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Backend.InhumacionCremacion.Entities.DTOs
+{
+    public class SeguimientoDto
+    {
+        public Guid Id { get; set; }
+        public DateTime FechaRegistro { get; set; }
+        public string Usuario { get; set; }
+        public string Estado { get; set; }
+        public Guid IdSolicitud { get; set; }
+        public string Observacion { get; set; }
+    }
+}
