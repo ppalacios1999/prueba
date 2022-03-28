27780d898306cc4089e561c15b6cbcf74e5469d4
AntonioFontalvo <acantillo@soaint.com>
2022-03-28 12:26:20 -0500

    Persistencia en ResumenSolicitud Agregada

---
+++ Backend.InhumacionCremacion.Entities/DTOs/ResumenSolicitudDTO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Backend.InhumacionCremacion.Entities.DTOs
+{
+    public class ResumenSolicitudDTO
+    {
+        public Guid IdSolicitud { get; set; }
+
+        public Guid NumeroTramite { get; set; }
+
+        public Guid EstadoSolicitud { get; set; }
+
+        public string NumeroLicencia { get; set; }
+
+        public string CorreoSolicitante { get; set; }
+
+        public string CorreoFuneraria { get; set; }
+
+        public string CorreoCementerio { get; set; }
+
+        public string TipoSeguimiento { get; set; }
+        public string NombreSolicitante { get; set; }
+        public string ApellidoSolicitante { get; set; }
+        public string NumeroDocumentoSolicitante { get; set; }
+        public Guid TipoDocumentoSolicitante { get; set; }
+    }
+}
