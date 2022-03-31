abf5309f47df44fa3f921049b09eedc9719629b7
DESKTOP-LD2P31H\Andres <avelez@soaint.com>
2022-03-30 23:45:22 -0500

    Implementacion Api Funeraria

---
+++ Backend.InhumacionCremacion.Entities/DTOs/DatosFunerariaDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Backend.InhumacionCremacion.Entities.DTOs
+{
+    public class DatosFunerariaDTO
+    {
+        //campo adicional update
+        public Guid IdDatosFuneraria { get; set; }
+        public bool? EnBogota { get; set; }
+        public bool? FueraBogota { get; set; }
+        public bool? FueraPais { get; set; }
+        public string Funeraria { get; set; }
+        public string OtroSitio { get; set; }
+        public string Ciudad { get; set; }
+        public Guid? IdPais { get; set; }
+        public Guid? IdDepartamento { get; set; }
+        public Guid? IdMunicipio { get; set; }
+    }
+}
---
+++ Backend.InhumacionCremacion.Repositories/Config/DatosFunerariaConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Backend.InhumacionCremacion.Entities.Models.InhumacionCremacion;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.InhumacionCremacion.Repositories.Config
+{
+    public static class DatosFunerariaConfig
+    {
+        public static void AddDatosFuneraria(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<DatosFuneraria>(entity =>
+            {
+                entity.HasKey(e => e.IdDatosFuneraria);
+
+                entity.ToTable("DatosFuneraria", "inhumacioncremacion");
+
+                entity.Property(e => e.IdDatosFuneraria).ValueGeneratedNever();
+
+                entity.Property(e => e.Funeraria).HasMaxLength(200);
+
+                entity.Property(e => e.Ciudad)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.OtroSitio).HasMaxLength(200);
+            });
+        }
+
+    }
+}
